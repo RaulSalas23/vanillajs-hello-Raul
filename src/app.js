ec6432d5a4eaba3ab9c54778c0a1694d10733333
RaulSalas23 <rast2305@gmail.com>
2024-09-20 19:46:50 +0000

    Primer Comit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function getRandomElement(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+  function generateExcuse() {
+    let excuse =
+      getRandomElement(who) +
+      " " +
+      getRandomElement(action) +
+      " " +
+      getRandomElement(what) +
+      " " +
+      getRandomElement(when) +
+      ".";
+    return excuse;
+  }
+
+  // se muestran las excusas
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
